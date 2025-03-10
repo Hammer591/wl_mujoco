a950b752599ca5d9f44cda8602106a84572df1f8
Hammer591 <2023113398@stu.hit.edu.cn>
2025-03-10 12:12:37 +0800

    加入了逆运动学、pd控制器，仿真输出过大

--- scripts/rl_sim_mj.py
+++ scripts/rl_sim_mj.py
@@ -34,14 +34,14 @@ class MuJoCoControl:
 
         # 观测配置
         self.obs_history = deque(maxlen=5)  # 存储最近5次观测
-        self.control_rate = 1000  # Hz
+        self.control_rate = 100  # Hz
         self.last_actions = np.zeros(6)     # 存储上次执行的动作
 
         # 状态参数
         self.gravity_vec = np.array([0, 0, -9.81])
         self.offset = 0
         self.l1 = 0.15
-        self.l2 = 0.30
+        self.l2 = 0.25
         self.pi = np.pi
 
         # 初始化 theta0, theta0_dot, L0 和 L0_dot
@@ -55,6 +55,9 @@ class MuJoCoControl:
         self.target_linear_velocity = 0.0  # 初始化目标线速度
         self.target_angular_vel = 0.0      # 初始化目标角速度
         self.target_z = 0.0                # 初始化目标z轴位置
+        
+        # 初始化 d_gains
+        self.d_gains = torch.tensor(np.zeros((1, 6)))
         
         # 初始化GLFW窗口
         glfw.init()
@@ -110,10 +113,10 @@ class MuJoCoControl:
 
     def _init_joint_info(self):
         """初始化关节和执行器信息"""
-        print("Total actuators:", self.mj_model.nu)
-        for i in range(self.mj_model.nu):
-            name = mujoco.mj_id2name(self.mj_model, mujoco.mjtObj.mjOBJ_ACTUATOR, i)
-            print(f"Actuator {i}: {name}")
+        # print("Total actuators:", self.mj_model.nu)
+        # for i in range(self.mj_model.nu):
+        #     name = mujoco.mj_id2name(self.mj_model, mujoco.mjtObj.mjOBJ_ACTUATOR, i)
+        #     print(f"Actuator {i}: {name}")
 
 
         self.motor_joint_names = [
@@ -143,7 +146,7 @@ class MuJoCoControl:
             mujoco.mj_name2id(self.mj_model, mujoco.mjtObj.mjOBJ_ACTUATOR, name)
             for name in self.actuator_names
         ]
-        print(f"Actuator IDs: {self.actuator_ids}")
+        # print(f"Actuator IDs: {self.actuator_ids}")
 
     def _key_callback(self, window, key, scancode, action, mods):
         """键盘控制回调"""
@@ -167,9 +170,9 @@ class MuJoCoControl:
     def _get_imu_data(self):
         """获取所有传感器数据"""
         return {
-            'quaternion': self.data.sensor('orientation').data.copy(),
-            'gyroscope': self.data.sensor('angular-velocity').data.copy(),
-            'accelerometer': self.data.sensor('linear-acceleration').data.copy(),
+            'quaternion': self.data.sensor('torso_quat').data.copy(),
+            'gyroscope': self.data.sensor('torso_angvel').data.copy(),
+            'accelerometer': self.data.sensor('torso_linacc').data.copy(),
             'motor_positions': [
                 self.data.sensor('lf0_Joint_p').data[0],
                 self.data.sensor('lf1_Joint_p').data[0],
@@ -214,11 +217,11 @@ class MuJoCoControl:
     
     def _get_obs(self):
         """生成当前观测向量（27维）"""
-        imu = self._get_imu_data()
-        
-        self.base_quat = torch.tensor(imu['quaternion'])
-        self.base_ang_vel = torch.tensor(imu['gyroscope'])
-        self.base_acc = torch.tensor(imu['accelerometer'])
+        self.torso_pos = self.data.sensordata[0:3]     # 位置 (x, y, z)
+        self.base_quat = self.data.sensordata[3:7]    # 四元数 (w, x, y, z)
+        self.base_linvel = self.data.sensordata[7:10] # 线速度 (vx, vy, vz)
+        self.base_ang_vel = self.data.sensordata[10:13] # 角速度 (wx, wy, wz)
+        self.base_acc = self.data.sensordata[13:16] # 线加速度 (ax, ay, az)
 
         self.angular_velocity = self.quat_rotate_inverse(
             self.base_quat, self.base_ang_vel
@@ -285,31 +288,121 @@ class MuJoCoControl:
         
         return np.concatenate(history)
 
-    def _apply_control(self, output):
+    def VMC(self, F, T):
+        theta0 = self.theta0 + self.pi / 2
+        t11 = self.l1 * torch.sin(
+            theta0 - self.theta1
+        ) - self.l2 * torch.sin(self.theta1 + self.theta2 - theta0)
+
+        t12 = self.l1 * torch.cos(
+            theta0 - self.theta1
+        ) - self.l2 * torch.cos(self.theta1 + self.theta2 - theta0)
+        t12 = t12 / self.L0
+
+        t21 = -self.l2 * torch.sin(self.theta1 + self.theta2 - theta0)
+
+        t22 = -self.l2 * torch.cos(self.theta1 + self.theta2 - theta0)
+        t22 = t22 / self.L0
+
+        T1 = t11 * F - t12 * T
+        T2 = t21 * F - t22 * T
+
+        return T1, T2
+    
+    def _apply_control(self, actions):
         """应用控制信号到执行器"""
+        self.action_scale_theta = 0.5
+        self.action_scale_l0 = 0.1
+        self.action_scale_vel = 10.0
+
+        self.l0_offset = 0.175
+        self.feedforward_force = 40.0  # [N]
+
+        self.kp_theta = 50.0  # [N*m/rad]
+        self.kd_theta = 3.0  # [N*m*s/rad]
+        self.kp_l0 = 900.0  # [N/m]
+        self.kd_l0 = 20.0  # [N*s/m]
+
+        theta0_ref = (
+            torch.cat(
+                (
+                    (actions[:, 0]).unsqueeze(1),
+                    (actions[:, 3]).unsqueeze(1),
+                ),
+                axis=1,
+            )
+            * self.action_scale_theta
+        )
+        l0_ref = (
+            torch.cat(
+                (
+                    (actions[:, 1]).unsqueeze(1),
+                    (actions[:, 4]).unsqueeze(1),
+                ),
+                axis=1,
+            )
+            * self.action_scale_l0
+        ) + self.l0_offset
+        wheel_vel_ref = (
+            torch.cat(
+                (
+                    (actions[:, 2]).unsqueeze(1),
+                    (actions[:, 5]).unsqueeze(1),
+                ),
+                axis=1,
+            )
+            * self.action_scale_vel
+        )
+
+        self.torque_leg = (
+            self.kp_theta * (theta0_ref - self.theta0) - self.kd_theta * self.theta0_dot
+        )
+        self.force_leg = self.kp_l0 * (l0_ref - self.L0) - self.kd_l0 * self.L0_dot
+        self.d_gains[:, 2] = 0.5
+        self.d_gains[:, 5] = 0.5
+        self.torque_wheel = self.d_gains[:, [2, 5]] * (
+            wheel_vel_ref - self.wheel_vel
+        )
+        T1, T2 = self.VMC(
+            self.force_leg + self.feedforward_force, self.torque_leg
+        )
+
+        torques = torch.cat(
+            (
+                T1[:, 0].unsqueeze(1),
+                T2[:, 0].unsqueeze(1),
+                self.torque_wheel[:, 0].unsqueeze(1),
+                -T1[:, 1].unsqueeze(1),
+                -T2[:, 1].unsqueeze(1),
+                self.torque_wheel[:, 1].unsqueeze(1),
+            ),
+            axis=1,
+        )
+
+        print(f"torques: {torques}")
+
+        k = 0.001
         # 电机位置控制
-        # motor_pos = torch.clamp(output[:4], -3.14, 3.14).tolist()
-        # self.data.ctrl[0] = motor_pos[0]  # lf0
-        # self.data.ctrl[1] = motor_pos[1]  # rf0
-        # self.data.ctrl[2] = motor_pos[2]  # lf1
-        # self.data.ctrl[4] = motor_pos[3]  # rf1
+        self.data.ctrl[0] = torques[0, 0].item() * k  # lf0
+        self.data.ctrl[1] = torques[0, 1].item() * k  # lf1
+        self.data.ctrl[3] = torques[0, 2].item() * k  # rf0
+        self.data.ctrl[4] = torques[0, 3].item() * k  # rf1
         
         # 轮毂速度控制
-        # wheel_vel = torch.clamp(output[4:6], -5, 5).tolist()
-        # self.data.ctrl[2] = wheel_vel[0]  # 左轮
-        # self.data.ctrl[5] = wheel_vel[1]  # 右轮
+        self.data.ctrl[2] = torques[0, 4].item() * k  # 左轮
+        self.data.ctrl[5] = torques[0, 5].item() * k  # 右轮
         
-        print(f"output: {output}")
+        # print(f"output: {output}")
 
-        self.data.ctrl[0] = 0  # lf0
-        self.data.ctrl[1] = 1.5  # lf1
-        self.data.ctrl[3] = 0  # rf0
-        self.data.ctrl[4] = 0  # rf1
-        self.data.ctrl[2] = 0  # 左轮
-        self.data.ctrl[5] = 0  # 右轮
+        # self.data.ctrl[0] = 0  # lf0
+        # self.data.ctrl[1] = 1.5  # lf1
+        # self.data.ctrl[3] = 0  # rf0
+        # self.data.ctrl[4] = 0  # rf1
+        # self.data.ctrl[2] = 0  # 左轮
+        # self.data.ctrl[5] = 0  # 右轮
 
         # 保存当前动作
-        self.last_actions = output.detach().numpy().copy()
+        self.last_actions = actions.detach().numpy().copy()
 
     def run(self):
         """主运行循环"""
@@ -329,50 +422,47 @@ class MuJoCoControl:
             viewer.cam.type = mujoco.mjtCamera.mjCAMERA_FIXED  # 固定摄像头模式
             while viewer.is_running():       # 控制频率保持
                 dt = 1.0 / self.control_rate
-
-                # 渲染
-                step = step + 1
-                step = step % 10
-                if step == 1:
-                    # 获取当前观测
-                    self.compute_L0_theta0()  # 更新 theta0 和 theta0_dot
-                    current_obs = torch.tensor(self._get_obs()).unsqueeze(0)  # 转换为 Tensor 并添加维度
-                    # print(f"current_obs shape: {current_obs.shape}")  # 打印 current_obs 的形状
-                    # torch.set_printoptions(threshold=torch.inf)
-                    # print("obs:", current_obs)
-
-                    # 获取历史观测（用于策略网络输入）
-                    obs_his = torch.tensor(self._get_obs_history()).unsqueeze(0)  # 转换为 Tensor 并添加维度
-                    # print(f"obs_his shape: {obs_his.shape}")  # 打印 obs_his 的形状
-                    
-                    # test_obs = torch.tensor(np.zeros(27, dtype=np.float32)).unsqueeze(0)
-                    # current_obs = test_obs
-
-                    # 策略推理
-                    with torch.no_grad():
-                        output_2 = self.policy_model_2(obs_his)
-                        # print(f"output_2 shape: {output_2.shape}")  # 打印 output_2 的形状
-                        combined_input = torch.cat([current_obs, output_2], dim=1)  # 拼接输入
-                        # print(f"combined_input shape: {combined_input.shape}")  # 打印 combined_input 的形状
-                        output_1 = self.policy_model_1(combined_input).squeeze(0)  # 确保 output_1 是一维张量
-                        # print(output_1)
-                
-                    # self._apply_control(output_1)
-                    # print(output_1.shape)
-                    # print(combined_input.shape)
-                self.data.ctrl[0] = 0  # lf0
-                self.data.ctrl[1] = 1  # lf1
-                self.data.ctrl[3] = 0  # rf0
-                self.data.ctrl[4] = 0  # rf1
-                self.data.ctrl[2] = 10  # 左轮
-                self.data.ctrl[5] = -10  # 右轮
-                mujoco.mj_step(self.mj_model, self.data) 
-                print(f"lf0: {self.data.ctrl[0]}, lf1: {self.data.ctrl[1]}, rf0: {self.data.ctrl[3]}, rf1: {self.data.ctrl[4]}, left: {self.data.ctrl[2]}, right: {self.data.ctrl[5]}")
-                # 渲染
-                viewer.sync()
-                while time.time() - last_time < dt:
+                while (time.time() - last_time) < dt:
                     time.sleep(0.001)
-                last_time = time.time()
+                # 获取当前观测
+                self.compute_L0_theta0()  # 更新 theta0 和 theta0_dot
+                current_obs = torch.tensor(self._get_obs()).unsqueeze(0)  # 转换为 Tensor 并添加维度
+                # print(f"current_obs shape: {current_obs.shape}")  # 打印 current_obs 的形状
+                torch.set_printoptions(threshold=torch.inf)
+                # print("obs:", current_obs)
+
+                # 获取历史观测（用于策略网络输入）
+                obs_his = torch.tensor(self._get_obs_history()).unsqueeze(0)  # 转换为 Tensor 并添加维度
+                # print(f"obs_his shape: {obs_his.shape}")  # 打印 obs_his 的形状
+                    
+                # test_obs = torch.tensor(np.zeros(27, dtype=np.float32)).unsqueeze(0)
+                # current_obs = test_obs
+
+                # 策略推理
+                with torch.no_grad():
+                    output_2 = self.policy_model_2(obs_his)
+                    # print(f"output_2 shape: {output_2.shape}")  # 打印 output_2 的形状
+                    combined_input = torch.cat([current_obs, output_2], dim=1)  # 拼接输入
+                    # print(f"combined_input shape: {combined_input.shape}")  # 打印 combined_input 的形状
+                    output_1 = self.policy_model_1(combined_input)
+                    # print(output_1)
+                
+                self._apply_control(output_1)
+                # print(output_1.shape)
+                # print(combined_input.shape)
+                # mujoco.mj_step(self.mj_model, self.data) 
+                # self.data.ctrl[0] = 0  # lf0
+                # self.data.ctrl[1] = 1  # lf1
+                # self.data.ctrl[3] = 0  # rf0
+                # self.data.ctrl[4] = 0  # rf1
+                # self.data.ctrl[2] = 15  # 左轮
+                # self.data.ctrl[5] = 15  # 右轮
+                mujoco.mj_step(self.mj_model, self.data) 
+                # print(f"lf0: {self.data.ctrl[0]}, lf1: {self.data.ctrl[1]}, rf0: {self.data.ctrl[3]}, rf1: {self.data.ctrl[4]}, left: {self.data.ctrl[2]}, right: {self.data.ctrl[5]}")
+                # 渲染
+                if step % 10 == 0:
+                    viewer.sync()
+                step += 1
 
             # glfw.poll_events()
 
